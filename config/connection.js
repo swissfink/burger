0bc30727bfa825349fe8e40273216639986388cc
Richard Fink <38076620+swissfink@users.noreply.github.com>
2019-04-24 19:57:36 -0400

    4/24 Upload 2

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "pUr5HLeFkp5E3Yk",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection= mysql.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "pUr5HLeFkp5E3Yk",
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
--- db/schema.sql
+++ db/schema.sql
@@ -7,8 +7,8 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
+	id INT AUTO_INCREMENT NOT NULL ,
+	burger_name VARCHAR(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
